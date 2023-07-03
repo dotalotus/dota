73b67724ef484c310a2d7cc9219e22d46549727a
Tyler Mueller <muelltyl@gmail.com>
2023-07-03 18:39:08 -0500

    adds language and gameversion options to queries

--- api/stratz/heroData.ts
+++ api/stratz/heroData.ts
@@ -1,17 +1,19 @@
 import { isErr } from "../../deps.ts";
-import { Hero } from "../../mod.ts";
+import { GameVersionID, Hero, LanguageID } from "../../mod.ts";
 import { StratzRequester } from "./requester.ts";
 
 interface Options {
-  gameVersionId?: `${number}`;
-  languageId?: `${number}`;
+  gameVersionId?: GameVersionID;
+  languageId?: LanguageID;
 }
 
 export async function fetchHeroData(
   options: Options = {},
 ) {
   const res = await StratzRequester.json("api/v1/Hero", {
-    searchParams: new URLSearchParams(Object.entries(options)),
+    searchParams: new URLSearchParams(
+      Object.entries(options).map(([key, value]) => [key, String(value)]),
+    ),
   });
   if (isErr(res)) return res;
   return Object.values(res as unknown as Record<string, Hero>);
--- api/stratz/itemData.ts
+++ api/stratz/itemData.ts
@@ -1,9 +1,18 @@
 import { isErr } from "../../deps.ts";
-import { Item } from "../../mod.ts";
+import { GameVersionID, Item, LanguageID } from "../../mod.ts";
 import { StratzRequester } from "./requester.ts";
 
-export async function fetchItemData() {
-  const response = await StratzRequester.json("api/v1/Item");
+interface Options {
+  gameVersionId?: GameVersionID;
+  lanaugageId?: LanguageID;
+}
+
+export async function fetchItemData(options: Options = {}) {
+  const response = await StratzRequester.json("api/v1/Item", {
+    searchParams: new URLSearchParams(
+      Object.entries(options).map(([key, value]) => [key, String(value)]),
+    ),
+  });
   if (isErr(response)) return response;
   return Object.values(response as unknown as Record<string, Item>);
 }
