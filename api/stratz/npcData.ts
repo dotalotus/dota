b970e4c144b0f5921b078241df7dfef77bdb1ecb
Tyler Mueller <muelltyl@gmail.com>
2023-07-03 18:57:54 -0500

    adds options to fetchNPCData

--- api/stratz/npcData.ts
+++ api/stratz/npcData.ts
@@ -1,9 +1,17 @@
 import { isErr } from "../../deps.ts";
-import { NPC } from "../../mod.ts";
+import { GameVersionID, NPC } from "../../mod.ts";
 import { StratzRequester } from "./requester.ts";
 
-export async function fetchNPCData() {
-  const response = await StratzRequester.json("api/v1/NPC");
+interface Options {
+  gameVersionId?: GameVersionID;
+}
+
+export async function fetchNPCData(options: Options = {}) {
+  const response = await StratzRequester.json("api/v1/NPC", {
+    searchParams: new URLSearchParams(
+      Object.entries(options).map(([key, value]) => [key, String(value)]),
+    ),
+  });
   if (isErr(response)) return response;
   return Object.values(response as unknown as Record<string, NPC>);
 }
