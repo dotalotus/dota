011b9453c2b1e6b2f533d9c7ede1783e31e67808
Tyler Mueller <muelltyl@gmail.com>
2023-07-05 16:28:48 -0500

    moves dota related types to Dota folder

--- types/api/dota/mod.ts
+++ types/api/dota/mod.ts
@@ -1,3 +1,5 @@
 export * from "./heroList.ts";
 export * from "./leaderboard.ts";
 export * from "./patchNotesList.ts";
+export * from "./matchHistory.ts";
+export * from "./matchHistoryBySequenceNum.ts";
--- types/api/steam/mod.ts
+++
@@ -1,2 +0,0 @@
-export * from "./matchHistory.ts";
-export * from "./matchHistoryBySequenceNum.ts";
